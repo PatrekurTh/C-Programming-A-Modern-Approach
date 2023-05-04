b8f6aab19d39124d2236b54aee59717c8d993d32
PatrekurTH <patrekurth@gmail.com>
2023-05-04 01:38:18 +0000

    Fix P12 Chapter 6

--- Chapter6/Projects/P12.c
+++ Chapter6/Projects/P12.c
@@ -1,6 +1,6 @@
 /*
  * Name: P12.c
- * Purpose: Approximates e up to n
+ * Purpose: Approximates e down to epsilon
  * Author: PatrekurTH
  */
 
@@ -8,24 +8,15 @@
 
 int main(void)
 {
-    int n, i, fac;
-    float term, epsilon, sum = 1.0f;
-
-    printf("Enter an integer: ");
-    scanf("%d", &n);
+    int i = 1, fac = 1;
+    float epsilon, term, sum = 1.0f;
 
     printf("Enter epsilon (small float): ");
     scanf("%f", &epsilon);
 
-    for (i = 1, fac = 1; i <= n; i++) {
-        term = 1.0f / (fac *= i);
-        if (term < epsilon)
-            break;
+    while ((term = 1.0f / (fac *= i++)) > epsilon)
         sum += term;
-    }
 
-        sum += 1.0f / (fac *= i);
-    
     printf("e approximation: %f\n", sum);
 
     return 0;
